299f6238de46ce6cb5bc8065acfc3d8e1b99c918
Sagar <worthless.dumboo@gmail.com>
2022-06-11 11:21:31 -0500

    Initial commit

---
+++ src/main/java/edu/miu/blog/BlogApplication.java
@@ -0,0 +1,20 @@
+package edu.miu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
