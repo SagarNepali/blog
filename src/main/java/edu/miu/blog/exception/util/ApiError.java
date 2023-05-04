b82b75a42b830d3a5cc022019f39d5c74570c7b8
Sagar Nepali <nepalisagar23@gmail.com>
2023-05-04 14:59:10 -0500

    Revert "Patch 2"

--- src/main/java/edu/miu/blog/exception/util/ApiError.java
+++ src/main/java/edu/miu/blog/exception/util/ApiError.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-/* Generic API Error response model */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
