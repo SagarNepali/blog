ca30350cc6e7ae968b53060b893ae6e4de7ecdd4
Sagar Nepali <worthless.dumboo@gmail.com>
2023-05-04 10:38:12 -0500

    Update BlogRestController.java

--- src/main/java/edu/miu/blog/controller/BlogRestController.java
+++ src/main/java/edu/miu/blog/controller/BlogRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-/*Controller*/
+/* REST Controller*/
 @RestController
 @RequestMapping("/api/v1/blogs")
 public class BlogRestController {
